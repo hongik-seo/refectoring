ad083cda58f0985de414a43af118e4f329a6e0f9
hongik <hongcha11@naver.com>
2022-07-02 12:57:55 +0900

    미싱넘버

--- leetCode/src/Main.java
+++ leetCode/src/Main.java
@@ -1,14 +1,7 @@
 class Main{
 
     public static void main(String[] args) {
-
-        ContainDuplicatedTwo containDuplicatedTwo = new ContainDuplicatedTwo();
-
-        int[] nums = {1,0,1,1};
-        int k = 1;
-
-        boolean result = containDuplicatedTwo.containsNearbyDuplicate(nums, k);
-
-        System.out.println(result);
+        int[] nums=null;
+        System.out.println(MissingNumber.missingNumber(nums));
     }
 }
